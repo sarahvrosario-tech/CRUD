428380fe8069d8363e16e7a9954dfa66fb686be5
Gabriel <gabriel.quadra@edu.es.senac.br>
2026-06-02 16:56:50 -0300

    envia usuario para MeuPerfil após login

--- MeuPerfil.xaml.cs
+++ MeuPerfil.xaml.cs
@@ -1,11 +1,46 @@
 ﻿using System.Windows;
+using CRUD.Modelos;
+using MySql.Data.MySqlClient;
 
 namespace CRUD;
 
 public partial class MeuPerfil : Window
 {
-    public MeuPerfil()
+    private Usuario UsuarioAtual;
+    public MeuPerfil(Usuario usuario)
     {
         InitializeComponent();
+        UsuarioAtual = usuario;
+        TxtNome.Text = UsuarioAtual.Nome;
+        TxtEmail.Text = UsuarioAtual.Email;
+        TxtUsername.Text = UsuarioAtual.Username;
+    }
+
+    private void BtnSalvar_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(TxtNome.Text))
+        {
+            MessageBox.Show("O campo NOME não pode estar vazio.");
+            TxtNome.Focus();
+            return;
+        }
+        
+        if (string.IsNullOrWhiteSpace(TxtEmail.Text))
+        {
+            MessageBox.Show("O campo EMAIL não pode estar vazio.");
+            TxtEmail.Focus();
+            return;
+        }
+        
+        if (string.IsNullOrWhiteSpace(TxtUsername.Text))
+        {
+            MessageBox.Show("O campo USERNAME não pode estar vazio.");
+            TxtUsername.Focus();
+            return;
+        }
+
+        using var conexao = new MySqlConnection(App.StringConexao);
+        const string query = "";
+
     }
 }
