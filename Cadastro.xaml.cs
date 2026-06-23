9fea1c06e5f8434b56e577c9cfab8761e186d90a
Sarah <sarahvrosario@gmail.com>
2026-06-23 15:26:26 -0300

    Refatora lógica de validaçao de campos textuais

--- Cadastro.xaml.cs
+++ Cadastro.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 using CRUD.Modelos;
 using MySql.Data.MySqlClient;
 
@@ -14,10 +15,24 @@ public partial class Cadastro : Window
 
     private void BtnCadastrar_OnClick(object sender, RoutedEventArgs e)
     {
-        if (string.IsNullOrWhiteSpace(TxtNome.Text) ||
-            string.IsNullOrWhiteSpace(TxtUsername.Text) ||
-            string.IsNullOrWhiteSpace(TxtEmail.Text) ||
-            string.IsNullOrWhiteSpace(TxtSenha.Password))
+        Dictionary<TextBox, string> caixasTexto = new()
+        {
+            { TxtNome, "Nome" },
+            { TxtEmail, "Email" },
+            { TxtUsername, "Username" },
+        };
+
+        foreach (var caixinha in caixasTexto)
+        {
+            if (string.IsNullOrWhiteSpace(caixinha.Key.Text)) 
+            {
+                MessageBox.Show($"O campo {caixinha.Value} não pode estar vazio.");
+                caixinha.Key.Focus();
+                return;
+            }
+        }
+
+        if (string.IsNullOrWhiteSpace(TxtSenha.Password))
         {
             MessageBox.Show("Todos os campos são obrigatórios.", "Erro!");
             return;
@@ -62,4 +77,6 @@ public partial class Cadastro : Window
             conexao.Close();
         }
     }
+    
+    
 }
